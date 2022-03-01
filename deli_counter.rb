cbe93f16087c4470dc0ee72802f07fb09abf361f
Nicole Steadman <nsteadman@gmail.com>
2022-03-01 13:56:52 -0800

    commit tree 36b60b815f78b712f324f66b4239ce2995ecac68

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(customers)
+    if customers.empty?
+        puts "The line is currently empty."
+    else
+        current_line = "The line is currently:"
+        customers.each.with_index(1) do |customer, number|
+            current_line << " #{number}. #{customer}"
+        end
+        puts current_line
+    end
+end
+
+def take_a_number(katz_deli, customer)
+    katz_deli << customer
+    puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(customer_array)
+    if customer_array.length < 1
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{customer_array.first}."
+    end
+    customer_array.shift
+end
